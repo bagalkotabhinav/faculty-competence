f14b7367b6b4b28b7be76f2206565f63563df7db
Abhinav Bagalkot <abhinavb.is21@rvce.edu.in>
2025-11-23 15:30:51 +0530

    Deleted redundant files and migrated DB to MySQL

---
+++ api/seed/seed_mysql.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const {
+  User,
+  Course,
+  Event,
+  Journal,
+  Conference,
+  Book,
+  Patent,
+  sequelize
+} = require('../models');
+
+const data = require('./data.json');
+
+(async () => {
+  try {
+    await sequelize.sync({ force: true });  // DROP + CREATE
+
+    await User.bulkCreate(data.users);
+
+    await Course.bulkCreate(data.courses);
+
+    await Event.bulkCreate(data.events);
+
+    await Journal.bulkCreate(data.journals);
+
+    await Conference.bulkCreate(data.conferences);
+
+    await Book.bulkCreate(data.books);
+
+    await Patent.bulkCreate(data.patents);
+
+    process.exit();
+  } catch (err) {
+    console.error('Seed failed:', err);
+    process.exit(1);
+  }
+})();
