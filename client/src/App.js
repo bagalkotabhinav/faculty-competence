249cce1fa7ec643f761b9a99543e755335cb54fb
Abhinav Bagalkot <abhinavb.is21@rvce.edu.in>
2026-03-08 10:16:54 +0530

    Dynamic Resources and Fixed Header div

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,33 @@
 import React from "react";
 import { Route, Routes, Navigate, useLocation } from "react-router-dom";
 
-/* Layout */
 import Header from "./components/layout/Header";
-
-/* Dashboard */
 import Dashboard from "./components/dashboard/Dashboard";
 
-/* User */
 import UserSignIn from "./components/user/UserSignIn";
 import UserSignUp from "./components/user/UserSignUp";
 import UserSignOut from "./components/user/UserSignOut";
 import UpdateUser from "./components/user/UpdateUser";
 
-/* Errors */
 import NotFound from "./components/errors/NotFound";
 import Forbidden from "./components/errors/Forbidden";
 import UnhandledError from "./components/errors/UnhandledError";
 
-/* Auth */
 import PrivateRoute from "./PrivateRoute";
 
-/* Generic Resource Components */
 import ResourceList from "./components/resource/ResourceList";
 import ResourceDetail from "./components/resource/ResourceDetail";
 import ResourceForm from "./components/resource/ResourceForm";
 import ResourceReport from "./components/resource/ResourceReport";
 
-/* All resources in the system */
-const resources = [
-  "courses",
-  "events",
-  "journals",
-  "conferences",
-  "books",
-  "patents"
-];
+import { resourceConfig } from "./resources/config";
 
 function App() {
 
   const location = useLocation();
 
-  /* Hide header on report pages */
+  const resources = Object.keys(resourceConfig);
+
   const isReportPage = location.pathname.endsWith("/report");
 
   return (
@@ -52,15 +38,12 @@ function App() {
       <main>
         <Routes>
 
-          {/* Redirect root */}
           <Route path="/" element={<Navigate replace to="/dashboard" />} />
 
-          {/* Public Routes */}
           <Route path="/signin" element={<UserSignIn />} />
           <Route path="/signup" element={<UserSignUp />} />
           <Route path="/signout" element={<UserSignOut />} />
 
-          {/* Private Routes */}
           <Route element={<PrivateRoute />}>
 
             <Route path="/dashboard" element={<Dashboard />} />
@@ -102,12 +85,10 @@ function App() {
 
           </Route>
 
-          {/* Error Pages */}
           <Route path="/notfound" element={<NotFound />} />
           <Route path="/forbidden" element={<Forbidden />} />
           <Route path="/error" element={<UnhandledError />} />
 
-          {/* Catch-all */}
           <Route path="*" element={<NotFound />} />
 
         </Routes>
--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -80,7 +80,7 @@ const Header = () => {
 
       {authUser && (
 
-        <div className={`fixed top-0 left-0 h-full w-64 bg-gray-900 p-4 transform transition-transform duration-300 ease-in-out ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+        <div className={`fixed top-0 left-0 h-full w-64 bg-gray-900 p-4 z-50 transform transition-transform duration-300 ease-in-out ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}`}>
 
           <CustomButton
             onClick={toggleSidebar}
