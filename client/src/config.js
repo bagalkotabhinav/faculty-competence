e78225c9e0980d9bb8fbe994e47058c47421ec12
Abhinav Bagalkot <abhinavb.is21@rvce.edu.in>
2026-05-09 21:10:58 +0530

    Added Nginx reverse proxy

--- client/src/config.js
+++ client/src/config.js
@@ -1,8 +1,5 @@
 const config = {
-  apiBaseUrl: 'http://localhost:5000/api', // Your deployed API base URL
+  apiBaseUrl: '/api', // Your deployed API base URL
 };
 
 export default config;
-
-// https://faculty-competence.onrender.com/api
-// http://localhost:5000/api
